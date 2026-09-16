17c081499a9eea7523443f3e0ab9c62e112fa5a8
Aaryan <abhishekaaryan20@gmail.com>
2026-09-16 08:36:47 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,10 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        h_set = {}
         if len(s) != len(t):
             return False
-        for i in range(len(s)):
-            h_set[s[i]] = h_set.get(s[i], 0) + 1
-            h_set[t[i]] = h_set.get(t[i], 0) - 1
 
-        return all(v == 0 for v in h_set.values())
+        count = {}
+
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+
+        for val in count.values():
+            if val != 0:
+                return False
+
+        return True
