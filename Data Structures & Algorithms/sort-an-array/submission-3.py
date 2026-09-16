faffd3080fd9a0ee659d4b015799040c6f418d28
Aaryan <abhishekaaryan20@gmail.com>
2026-09-16 16:59:32 +0530

    Update: sort-an-array - submission-3

--- Data Structures & Algorithms/sort-an-array/submission-3.py
+++ Data Structures & Algorithms/sort-an-array/submission-3.py
@@ -1,11 +1,28 @@
 class Solution:
-   def sortArray(self, nums: List[int]) -> List[int]:
-    if len(nums) <= 1:
-        return nums
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        mid_index=len(nums)//2
+        left=self.sortArray(nums[:mid_index])
+        right=self.sortArray(nums[mid_index:])
+        return self.merge(left,right)      
 
-    pivot = nums[len(nums) // 2]
-    left = [x for x in nums if x < pivot]
-    middle = [x for x in nums if x == pivot]
-    right = [x for x in nums if x > pivot]
-
-    return self.sortArray(left) + middle + self.sortArray(right)
+    def merge(self,list1,list2):
+        result=[]
+        i,j=0,0
+        while i<len(list1) and j < len(list2):
+            if list1[i]<=list2[j]:
+                result.append(list1[i])
+                i=i+1
+            else:
+                result.append(list2[j])
+                j=j+1
+        while i< len(list1):
+            result.append(list1[i])
+            i=i+1
+        
+        while j < len(list2):
+            result.append(list2[j])
+            j=j+1
+        
+        return result
